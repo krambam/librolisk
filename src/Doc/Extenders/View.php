1759ba52fd74be4d8b1a54e9ffc6e8b13043a277
Nicolas Wanner <cutslade@hotmail.com>
2016-09-13 00:02:05 +0200

    doc

---
+++ src/Doc/Extenders/View.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Medialisk\Librolisk\Doc\Extenders;
+
+/**
+ *
+ * @property \Pimcore\Model\Document\Page document
+ * @method \Pimcore\View\Helper\PimcoreNavigation pimcoreNavigation($activeDocument = null, $navigationRootDocument = null, $htmlMenuIdPrefix = null, $pageCallback = null, $cache = true)
+ *
+ *
+ * @method \Pimcore\Model\Document\Tag\Area area(string $name, array $options = array())
+ * @method \Pimcore\Model\Document\Tag\Areablock areablock(string $name, array $options = array())
+ * @method \Pimcore\Model\Document\Tag\Block block(string $name, array $options = array())
+ * @method \Pimcore\Model\Document\Tag\Checkbox checkbox(string $name, array $options = array())
+ * @method \Pimcore\Model\Document\Tag\Date date(string $name, array $options = array())
+ * @method \Pimcore\Model\Document\Tag\Href href(string $name, array $options = array())
+ * @method \Pimcore\Model\Document\Tag\Image image(string $name, array $options = array())
+ * @method \Pimcore\Model\Document\Tag\Input input(string $name, array $options = array())
+ * @method \Pimcore\Model\Document\Tag\Link link(string $name, array $options = array())
+ * @method \Pimcore\Model\Document\Tag\MultiHref multiHref(string $name, array $options = array())
+ * @method \Pimcore\Model\Document\Tag\MultiSelect multiSelect(string $name, array $options = array())
+ * @method \Pimcore\Model\Document\Tag\Numeric numeric(string $name, array $options = array())
+ * @method \Pimcore\Model\Document\Tag\Pdf pdf(string $name, array $options = array())
+ * @method \Pimcore\Model\Document\Tag\Renderlet renderlet(string $name, array $options = array())
+ * @method \Pimcore\Model\Document\Tag\Resource resource(string $name, array $options = array())
+ * @method \Pimcore\Model\Document\Tag\Select select(string $name, array $options = array())
+ * @method \Pimcore\Model\Document\Tag\Snippet snippet(string $name, array $options = array())
+ * @method \Pimcore\Model\Document\Tag\Table table(string $name, array $options = array())
+ * @method \Pimcore\Model\Document\Tag\TagInterface tagInterface(string $name, array $options = array())
+ * @method \Pimcore\Model\Document\Tag\TextArea textArea(string $name, array $options = array())
+ * @method \Pimcore\Model\Document\Tag\Video video(string $name, array $options = array())
+ * @method \Pimcore\Model\Document\Tag\Wysiwyg wysiwyg(string $name, array $options)
+ *
+ */
+class View extends \Pimcore\View
+{
+
+}
